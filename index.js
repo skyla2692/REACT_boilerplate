26da2e995ed447f816cf5a180b27733a07d4aee5
skylalee <reloxo99@gmail.com>
2021-03-09 15:52:50 +0900

    Login function

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = 5000  // 5000변 port를 백서버로 설정한다
 const config = require('./config/key');
 
 const bodyParser = require('body-parser');
-const {USER} = require("./models/user");
+const cookieParser = require('cookie-parser');
+const {User} = require("./models/User");
 
 
 // application/x-ww-form-urlencoded
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // application/json
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -33,12 +34,49 @@ app.get('/', (req, res) => {
 
 app.post('/register', (req, res) => {
   // 회원가입 시 필요한 정보들을 client에서 가져오면 그것들을 데베에 넣어준다
-  const user = new USER(req.body)
+  const user = new User(req.body)
 
   user.save((err, userInfo) => {
     if(err) return res.json({ success: false, err})
     return res.status(200).json({
       success: true
+    })
+  })
+})
+
+
+app.post('/api/users/login', (req, res) => {
+
+  // console.log('ping')
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+
+    // console.log('user', user)
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // console.log('err',err)
+
+      // console.log('isMatch',isMatch)
+
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+      //비밀번호 까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
     })
   })
 })
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const user_schema = mongoose.Schema({
     name: {
@@ -41,21 +42,63 @@ user_schema.pre('save', function(next){
     // 비밀번호를 암호화 하기
 
     var user = this;    // == schema
-
     if(user.isModified('password')){
         bcrypt.genSalt(saltRounds, function(err, salt){
-            if(err) return next(err)
+            if(err) return next(err);
             bcrypt.hash(user.password, salt, function(err, hash){
-                if(err) return next(err)
+                if(err) return next(err);
                 user.password = hash
 
                 next()
             })
         })
     }
+    else{
+        next()
+    }
 })
 
+user_schema.methods.comparePassword = function (plainPassword, cb) {
 
-const USER = mongoose.model('USER', user_schema);
+    //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+}
 
-module.exports = {USER};
+user_schema.methods.generateToken = function (cb) {
+    var user = this;
+    // console.log('user._id', user._id)
+
+    // jsonwebtoken을 이용해서 token을 생성하기 
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token 
+    // -> 
+    // 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
+
+
+user_schema.statics.findByToken = function(token, cb) {
+    var user = this;
+    // user._id + ''  = token
+    //토큰을 decode 한다. 
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에 
+        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
+const User = mongoose.model('User', user_schema);
+
+module.exports = {User};
