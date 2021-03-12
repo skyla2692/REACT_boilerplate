081e129d99e7fec835f0e94dbfd5558c56435495
skylalee <reloxo99@gmail.com>
2021-03-12 10:37:43 +0900

    client file added and reorganized

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/register">
+            <RegisterPage />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
+)}
+
+export default App;
+
+
+
+
+
